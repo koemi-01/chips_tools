3369ff6230d7d547794a40ab3e0588e43fb034e5
koemi-01 <1246909122@qq.com>
2025-02-09 15:48:27 -0500

    Loading DAT files

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,18 @@
 import cc_dat_utils
 
 #Part 1
-input_dat_file = "data/pfgd_test.dat"
+# input_dat_file = "data/pfgd_test.dat"
+input_dat_file = "chips_tools\data\pfgd_test.dat"
+
 
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+# load level
+levelPack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
+# print output
+output = str(levelPack)
+print(output)
+
